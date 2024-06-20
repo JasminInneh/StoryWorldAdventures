5da22812c651bde6fe4a7ad70666e1da49c8b2b7
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-06-20 18:13:39 +0000

    added the footer component as well as the global styles

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export const Footer = () => {
+  return (
+    <footer className="text-center text-lg-start">
+      <Container className="p-4">
+        <Row>
+          <Col lg={6} className="mb-4 mb-lg-0">
+            <h5 className="text-uppercase">Contact Us</h5>
+            <p>
+              Miami, Florida <br />
+              Phone: +1 234 567 890
+              <br />
+              Email: jasmininneh@gmail.com
+            </p>
+          </Col>
+          <Col lg={6}>
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#!" className="text-dark">
+                  Follow us on Instagram
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">
+                  Terms of Service
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">
+                  Cookie Policy
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
