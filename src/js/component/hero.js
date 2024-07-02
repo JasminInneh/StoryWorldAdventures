b1ff9a72b164fb4d595a4fec89cd37cfd7739dce
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-07-02 01:29:24 +0000

    cleaned and organized the code in the header

--- src/js/component/hero.js
+++ src/js/component/hero.js
@@ -5,12 +5,25 @@ import hero from "../../styles/hero.css";
 export const Hero = () => {
   return (
     <div className="banner">
-      <Container fluid>
-        <div className="banner-content">
-          <h1>Welcome to Story World Adventures</h1>
-          <p>Explore, Learn and Imagine in Every Chapter</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 d-flex flex-column align-items-start">
+            <h1>Embark on a Journey of Imagination</h1>
+            <p>
+              Discover enchanting tales and adventures in our digital library
+              designed for young readers.
+            </p>
+            <div className="d-flex justify-content-center">
+              <button type="button" className="btn btn-warning mx-2">
+                Start Reading
+              </button>
+              <button type="button" className="btn btn-success mx-2">
+                Join Now
+              </button>
+            </div>
+          </div>
         </div>
-      </Container>
+      </div>
     </div>
   );
 };
