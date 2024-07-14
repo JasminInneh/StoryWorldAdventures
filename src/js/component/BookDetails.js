9c7028b55e093ea582924a8e27b1026cfbe6575e
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-07-14 02:53:36 +0000

    updated the BookDetails component to use Bootstrap cards and accept props for the book details instead of fetching them based on bookId

--- src/js/component/BookDetails.js
+++ src/js/component/BookDetails.js
@@ -1,26 +1,31 @@
-import React, { useContext, useEffect } from 'react';
-import { Context } from '../store/appContext';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const BookDetails = () => {
-  const { store, actions } = useContext(Context);
-  const { bookId } = useParams();
-
-  useEffect(() => {
-    actions.fetchBookDetails(bookId);
-  }, [bookId]);
-
-  if (!store.bookDetails) {
-    return <div>Loading...</div>;
-  }
-
+const BookDetails = ({ title, author, cover, description }) => {
   return (
-    <div>
-      <h1>{store.bookDetails.title}</h1>
-      <p>{store.bookDetails.description}</p>
-      {/* Add more details as needed */}
+    <div className="card mb-3" style={{ maxWidth: '540px' }}>
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={cover} className="img-fluid rounded-start" alt={title} />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text"><small className="text-muted">{author}</small></p>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
+BookDetails.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  cover: PropTypes.string,
+  description: PropTypes.string
+};
+
 export default BookDetails;
+
